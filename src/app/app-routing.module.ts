4d933a6b4d803cc5ceb1570d4884ad4dccfc558a
Alok Jha <alokjha0209@gmail.com>
2021-12-14 16:30:13 +0530

    addedheader and home section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { ServicesComponent } from './components/services/services.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'services', component: ServicesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
